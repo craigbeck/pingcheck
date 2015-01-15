6c8eab953f9d265536e7f9a68499d52a0505fade
Craig Beck <luckybonza@gmail.com>
2015-01-14 20:45:31 -0800

    added tests; fixed stats and added percentiles

--- test/stats.spec.js
+++ test/stats.spec.js
@@ -12,4 +12,21 @@ describe("Stats module", function () {
       expect(isNaN(Stats.avg([]))).to.be.true;
     });
   });
+
+  describe("pct", function () {
+    it("should calculate percentiles", function () {
+      var data = [3, 5, 7, 9];
+      expect(Stats.pct(data, 0.00)).to.equal(3);
+      expect(Stats.pct(data, 0.25)).to.equal(5);
+      expect(Stats.pct(data, 0.50)).to.equal(7);
+      expect(Stats.pct(data, 0.75)).to.equal(9);
+    });
+
+    it("should return undefined for invalid percentiles", function () {
+      var data = [3, 4];
+      // cant be in 75% percentile with population of 2
+      expect(Stats.pct(data, 0.75)).to.equal(undefined);
+      expect(Stats.pct(data, 0.50)).to.equal(4);
+    });
+  });
 });
