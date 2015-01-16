de10d6afc90076fe5d13c610afe175740cf0e3d7
Craig Beck <luckybonza@gmail.com>
2015-01-16 12:45:19 -0800

    remove unused tasks + libs

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,8 +1,5 @@
 var gulp = require("gulp");
-var watch = require("gulp-watch");
-var plumber = require("gulp-plumber");
 var mocha = require("gulp-mocha");
-var jasmine = require("gulp-jasmine");
 var jshint = require("gulp-jshint");
 var stylish = require("jshint-stylish");
 var runSequence = require("run-sequence");
@@ -28,11 +25,6 @@ gulp.task("mocha", function () {
     });
 });
 
-gulp.task("jasmine", function (){
-  return gulp.src("./test")
-    .pipe(jasmine());
-});
-
 gulp.task("test", ["mocha"]);
 
 gulp.task("jshint", function (){
@@ -40,17 +32,6 @@ gulp.task("jshint", function (){
     .pipe(jshint(_jsonCfg(".jshint.json")))
     .pipe(jshint.reporter(stylish))
     .pipe(jshint.reporter("fail"));
-});
-
-gulp.task("watch", function () {
-  watch(["*.js", "lib/*.js", "test/*.js"], function () {
-    gulp.start("test");
-  });
-});
-
-gulp.task("test:watch", function () {
-  gulp.watch(["lib/*.js", "test/*.js"], ["test"])
-    .on("error", function (err) {});
 });
 
 gulp.task("check", function (done) {
