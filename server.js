3742f75bcdee112ff457a78773395fa3d63e2f69
Craig Beck <luckybonza@gmail.com>
2015-02-04 20:14:23 -0800

    add basic persistence to mongo

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ var passport = require("passport");
 var postal = require("postal");
 var Rollbar = require("rollbar");
 var url = require("url");
+var mongo = require("promised-mongo");
 
 var Agent = require("./lib/agent");
 var pkg = require("./package.json");
@@ -19,20 +20,45 @@ var strategy = require("./lib/auth");
 var app = express().http().io();
 var connections = {};
 
-app.configure(function () {
-  this.use(BodyParser.json());
-  this.use(express.cookieParser());
-  this.use(express.session({secret: "h0lyS3kretHand5h@k3Ba7m4n!"}));
-  this.use(passport.initialize());
-  this.use(passport.session());
 
-  this.use(app.router);
+var requestId = function (req, res, next) {
+  req._requestId = nextReqId();
+  res.setHeader("X-RequestId", req._requestId);
+  next();
+};
 
-  if (process.env.ROLLBAR_ACCESS_TOKEN) {
-    Rollbar.init(process.env.ROLLBAR_ACCESS_TOKEN);
-    app.use(Rollbar.errorHandler(process.env.ROLLBAR_ACCESS_TOKEN));
-  }
-});
+var requestLogger = function (req, res, next) {
+  req._startAt = process.hrtime();
+  req._startTime = new Date();
+  onFinished(res, function () {
+    var diff = process.hrtime(req._startAt);
+    var ms = diff[0] * 1e3 + diff[1] * 1e-6;
+    var elapsed = ms.toFixed(3);
+    console.log("%s [%s] - %s %s %s (%sms)",
+                req._startTime.valueOf(),
+                req._requestId,
+                req.method,
+                req.path,
+                res.statusCode,
+                elapsed);
+  });
+  next();
+}
+
+app.use(BodyParser.json());
+app.use(express.cookieParser());
+app.use(express.session({secret: "h0lyS3kretHand5h@k3Ba7m4n!"}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(requestId);
+app.use(requestLogger);
+app.use(app.router);
+
+if (process.env.ROLLBAR_ACCESS_TOKEN) {
+  Rollbar.init(process.env.ROLLBAR_ACCESS_TOKEN);
+  app.use(Rollbar.errorHandler(process.env.ROLLBAR_ACCESS_TOKEN));
+}
 
 app.io.on("connection", function (socket) {
   var pingClients, tid;
@@ -88,7 +114,7 @@ var toJson = function (obj) {
   };
   var meta = {
     _links: {
-      self: (process.env.HEROKU_URL || "/") + "agents/" + obj.id
+      self: (process.env.HEROKU_URL || "/") + "agents/" + obj.hash
     }
   };
   return _.extend(meta, { stats: stats }, attrs);
@@ -153,34 +179,7 @@ if (process.env.KEEN_WRITE_KEY && process.env.KEEN_PROJECT_ID) {
   process.nextTick(pulse);
 }
 
-app.use(function (req, res, next) {
-  req._startAt = process.hrtime();
-  req._startTime = new Date();
-  onFinished(res, function () {
-    var diff = process.hrtime(req._startAt);
-    var ms = diff[0] * 1e3 + diff[1] * 1e-6;
-    var elapsed = ms.toFixed(3);
-    console.log("%s [%s] - %s %s %s (%sms)",
-                req._startTime.valueOf(),
-                req._requestId,
-                req.method,
-                req.path,
-                res.statusCode,
-                elapsed);
-  });
-  next();
-});
-
-var requestId = function (req, res, next) {
-  req._requestId = nextReqId();
-  res.setHeader("X-RequestId", req._requestId);
-  next();
-};
-
-app.use(requestId);
-
 app.get("/", function (req, res) {
-  console.log("SESSION", req.session);
   if (req.accepts("html")) {
     fs.readFile("./app/index.html", function (err, data) {
       if (err) {
@@ -202,7 +201,9 @@ app.get("/", function (req, res) {
 var agents = {};
 
 app.get("/agents", function (req, res) {
-  res.send({ ok: true, agents: _.map(_.toArray(agents), toJson) });
+  db.agents.find().toArray().then(function (agents) {
+    res.send({ ok: true, agents: _.map(_.toArray(agents), toJson) });
+  });
 });
 
 var isValidUri = function (str) {
@@ -217,46 +218,39 @@ var isValidUri = function (str) {
   }
 };
 
-app.post("/agents", function (req, res) {
-  var uri = req.body.url;
-  try {
-    url.parse(uri);
-  } catch (e) {
-    return res.status(400).send({
-      ok: false,
-      error: "Bad Request",
-      message: "Invalid URL",
-      url: uri
-    });
-  }
-  var interval = parseInt(req.body.interval || 60, 10);
-  if (!uri) {
-    return res.status(400).send({ ok: false, error: "Bad Request" });
-  }
-  var agent = new Agent(uri, { interval: interval });
-  if (agents[agent.id]) {
-    return res.status(200).send({ ok: true, data: toJson(agents[agent.id]) });
-  }
+var db = mongo(process.env.MONGOLAB_URI, ["agents", "activity"]);
+db.activity.ensureIndex("agentId");
 
-  agents[agent.id] = agent;
-  evtChannel.publish("url.added", { id: agent.id, href: agent.href });
-
+var initializeAgent = function (agent) {
   agent.on("started", function (obj) {
     console.log("%s - STARTED %s %s",
                 new Date().valueOf(),
-                obj.id, obj.href);
+                obj.hash, obj.href);
   });
 
   agent.on("stopped", function (obj) {
     console.log("%s - STOPPED %s %s",
                 new Date().valueOf(),
-                obj.id, obj.href);
+                obj.hash, obj.href);
+  });
+
+  agent.on("state:changed", function (data) {
+    process.nextTick(function () {
+      db.agents.update(
+        { _id: data.hash },
+        { $set: { state: data.state, updated: new Date() } }
+      ).then(function () {
+        console.log("update", data.hash, "OK");
+      }, function (err) {
+        console.error("update", data.hash, err);
+      });
+    });
   });
 
   agent.on("data", function (data) {
     console.log("%s - PING %s %s %s (%sms)",
                 new Date().valueOf(),
-                data.obj.id, data.obj.href, data.status, data.msec);
+                data.obj.hash, data.obj.href, data.status, data.msec);
     evtChannel.publish("url.checked", {
       id: agent.id,
       href: agent.href,
@@ -265,26 +259,133 @@ app.post("/agents", function (req, res) {
       timestamp: data.startTime,
       responseTime: data.msec
     });
+
+    var doc = {
+      agentId: agent.hash,
+      timestamp: data.startTime,
+      statusCode: data.status,
+      msec: data.msec
+    };
+
+    db.activity.insert(doc).then(null, function (err) {
+      console.error("%s - ERR failed to save activity",
+                    new Date().valueOf(),
+                    data.obj.hash, data.obj.href, data.status, data.msec);
+    });
   });
 
   agent.on("error", function (data) {
     console.log("%s - PING %s %s %s (%sms)",
                 new Date().valueOf(),
-                data.obj.id, data.obj.href, data.error, data.msec);
-    evtChannel.publish("url.added", { id: agent.id, href: agent.href });
+                data.obj.hash, data.obj.href, data.error, data.msec);
     agent.stop();
   });
 
   agent.start();
-  res.status(201).send({ ok: true, data: toJson(agent) });
+};
+
+db.agents.find()
+  .toArray()
+  .then(function (docs) {
+    if (!docs.length) {
+      console.log("no agents to initialize");
+    }
+    docs.forEach(function (doc) {
+      var agent = new Agent(doc.href, { interval: doc.interval });
+      initializeAgent(agent);
+      console.log("init", agent.hash, agent.href);
+      agents.push(agent);
+    });
+  });
+
+app.post("/agents", function (req, res) {
+  var uri;
+  try {
+    uri = url.parse(req.body.url);
+  } catch (e) {
+    return res.status(400).send({
+      ok: false,
+      error: "Bad Request",
+      message: "Invalid URL",
+      url: req.body.url
+    });
+  }
+  var hash = Agent.idHash(url.format(uri));
+  db.agents.findOne({ _id: hash })
+    .then(function (doc) {
+      if (doc) {
+        // found it!
+        console.log("found doc");
+        return res.status(400).send({ ok: false, error: "already exists" });
+      }
+
+      var interval = parseInt(req.body.interval || 60, 10);
+      if (!uri) {
+        return res.status(400).send({ ok: false, error: "Bad Request" });
+      }
+      var agent = new Agent(url.format(uri), { interval: interval });
+      if (agents[agent.id]) {
+        return res.status(200).send({ ok: true, data: toJson(agents[agent.id]) });
+      }
+
+      var newAgent = {
+        _id: agent.hash,
+        interval: agent.interval,
+        hash: agent.hash,
+        href: url.format(uri),
+        url: uri,
+        state: agent.state
+      };
+
+      db.agents.insert(newAgent).then(function (doc) {
+        agents[agent.id] = agent;
+        evtChannel.publish("url.added", { id: agent.id, href: agent.href });
+
+        initializeAgent(agent);
+
+        res.status(201).send({ ok: true, data: doc });
+      }, function (err) {
+        console.error(err);
+        res.status(500).send({ ok: false, error: err });
+      });
+    });
 });
 
 app.get("/agents/:hash", function (req, res) {
-  var url = agents[req.params.hash];
-  if (!url) {
-    return req.status(404).send({ ok: false, error: "Not Found" });
+  db.agents.findOne({ _id: req.params.hash }).then(function (doc) {
+    if (!doc) {
+      return res.status(404).send({ ok: false, error: "Not Found" });
+    }
+    var agent = doc;
+    db.activity
+      .find({ agentId: req.params.hash })
+      .toArray()
+      .then(function (docs) {
+        var activity = docs;
+        var obj = _.extend({ }, agent, { history: activity } );
+        res.send({ ok: true, agent: toJson(obj) });
+      }, function (err) {
+        return res.status(500).send({ ok: false, error: err });
+      });
+
+  }, function (err) {
+    return res.status(500).send({ ok: false, error: err });
+  });
+});
+
+app.put("/agents/:hash", function (req, res) {
+  var agent = agents[req.params.hash];
+  if (!agent) {
+    return res.status(404).send({ ok: false, error: "Not Found" });
   }
-  res.send({ ok: true, url: toJson(url) });
+  var state = req.body.state;
+  if (state != "ready") {
+    return res.status(400).send({ ok: false, error: "Missing State" });
+  }
+  agent.reset();
+  process.nextTick(function () {
+    agent.start();
+  });
 });
 
 app.delete("/agents/:hash", function (req, res) {
