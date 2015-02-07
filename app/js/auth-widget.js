2ef5d29d3afe5100cb3371eba46bebd598709656
Craig Beck <luckybonza@gmail.com>
2015-02-07 10:51:34 -0800

    make callback dynamic so it works on heroku + localhost

--- app/js/auth-widget.js
+++ app/js/auth-widget.js
@@ -1,15 +1,16 @@
 var Auth0Widget;
+var widget;
 
 try {
   Auth0Widget = require("Auth0Widget");
+  var widget = new Auth0Widget({
+    domain:         "app33164094.auth0.com",
+    clientID:       "rakzQVY18vG1GOEIhIhWMKIt6Gq2PeVz",
+    callbackURL:    document.baseURI + "callback"
+  });
 } catch (e) {
-  Auth0Widget = function () {};
+  widget = function () {};
 }
 
-var widget = new Auth0Widget({
-  domain:         "app33164094.auth0.com",
-  clientID:       "rakzQVY18vG1GOEIhIhWMKIt6Gq2PeVz",
-  callbackURL:    "http://localhost:5000/callback"
-});
 
 module.exports = widget;
